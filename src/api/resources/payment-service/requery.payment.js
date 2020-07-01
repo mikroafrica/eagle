9c4dfd7d3a8c39189ebe111e25c4b150c26f8a26
Meshileya seun <meshileyaseun@gmail.com>
2020-07-01 12:34:32 +0100

    reprocess incomplete handshake payment

---
+++ src/api/resources/payment-service/requery.payment.js
@@ -0,0 +1,85 @@
+import cron from "cron";
+const CronJob = cron.CronJob;
+
+import moment from "moment";
+import logger from "../../../logger";
+import {
+  morning,
+  night,
+  TransactionMessagingType,
+  TransactionStatus,
+} from "../commons/model";
+import { PaymentServiceClient } from "../../db";
+import type { TransactionMessaging } from "../commons/model";
+import { PAYMENT_EMITTER } from "./requery.payment.event";
+import paymentEvent from "./requery.payment.event";
+
+function reQueryPendingTransfer(callback) {
+  const handShakeStatus = "PUBLISHED_SUCCESSFUL";
+  const query = {
+    text:
+      "SELECT * FROM transactions tnx " +
+      "WHERE handshake_status = $1 AND tnx.status = $2 AND tnx.type = $3" +
+      "AND tnx.time_created >= $4 AND tnx.time_created <= $5 ",
+
+    values: [
+      handShakeStatus,
+      TransactionStatus.SUCCESS,
+      TransactionMessagingType.WALLET_TOP_UP,
+      morning(),
+      night(),
+    ],
+  };
+
+  const client = PaymentServiceClient();
+
+  client
+    .query(query)
+    .then((response) => {
+      const results = response.rows;
+      logger.info(
+        `Total number of queried payment results is [${results.length}]`
+      );
+      const transactionMessaging: TransactionMessaging[] = results.map(
+        function (data) {
+          return handleWalletTopUp(data);
+        }
+      );
+      callback(transactionMessaging);
+
+      client.end();
+    })
+    .catch((error) => {
+      logger.error(
+        `error occurred while fetching pending with error [${error}]`
+      );
+    });
+}
+
+export const RetryPaymentJob = (): CronJob => {
+  return new CronJob("* */1 * * * *", function () {
+  // return new CronJob("0 */10 * * * *", function () {
+    const formattedDate = moment.tz("Africa/Lagos");
+    logger.info(`::: re-processing for payment started ${formattedDate} :::`);
+
+    reQueryPendingTransfer(function (
+      transactionMessaging: TransactionMessaging[]
+    ) {
+      paymentEvent.emit(PAYMENT_EMITTER, transactionMessaging);
+    });
+  });
+};
+
+function handleWalletTopUp(data): TransactionMessaging {
+  const callbackResponse = data.callback_response.callback_response;
+  return {
+    paymentReference: data.payment_reference,
+    amount: data.amount,
+    paymentStatus: TransactionStatus.SUCCESS,
+    email: callbackResponse.customer.email,
+    accountNumber: callbackResponse.accountDetails.accountNumber,
+    vendor: data.vendor,
+    type: TransactionMessagingType.WALLET_TOP_UP,
+    callbackResponse: callbackResponse,
+  };
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import logger from "./logger";
 import server from "./server";
 import { RetryTransferJob } from "./api/resources/transaction-service/requery/transfer";
 import { RetryWalletTopUpJob } from "./api/resources/transaction-service/requery/walletop-up";
+import { RetryPaymentJob } from "./api/resources/payment-service/requery.payment";
 
 // handle all uncaught errors
-process.on("uncaughtException", function (err) {
-  logger.error(`uncaught error has been fired with Error: ${err}`);
-});
+// process.on("uncaughtException", function (err) {
+//   logger.error(`uncaught error has been fired with Error: ${err}`);
+// });
 
 RetryTransferJob().start();
 RetryWalletTopUpJob().start();
+RetryPaymentJob().start();
 
 const port = process.env.PORT || 3000;
 server.listen(port, function () {
