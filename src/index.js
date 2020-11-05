9e55e196fb83b80f85f066427128f72523d59cad
Meshileya seun <meshileyaseun@gmail.com>
2020-11-05 13:49:55 +0100

    update

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ RetryPaymentWalletTopUpJob().start();
 RetryPaymentTerminalJob().start();
 RetryBillsJob().start();
 
-PreviousDayTargetReportJob().start();
+// PreviousDayTargetReportJob().start();
 
 const port = process.env.PORT || 3000;
 server.listen(port, function () {
