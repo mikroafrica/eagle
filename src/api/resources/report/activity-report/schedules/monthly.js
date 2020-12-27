3b5cfe868caab27724444e76743177798cb9dea1
Paul Ofili <paul@mikro.africa>
2020-12-27 11:18:48 +0100

    Updated job start times

--- src/api/resources/report/activity-report/schedules/monthly.js
+++ src/api/resources/report/activity-report/schedules/monthly.js
@@ -13,10 +13,10 @@ const pastDayInMorning = firstDayOfLastMonth();
 const pastDayAtNight = lastDayOfLastMonth();
 const schedule = "month";
 
-//Generates user's activity report for the past month and send to email every 1st day of the new month by 12:00 AM
+//Generates user's activity report for the past month and send to email every 1st day of the new month by 1:00 AM
 export const ActivityReportMonthlyJob = (): CronJob => {
   return new CronJob(
-    "0 0 0 1 * *",
+    "0 0 1 1 * *",
     function () {
       const formattedDate = moment.tz("Africa/Lagos");
       logger.info(`::: Monthly Activity report @ ${formattedDate} :::`);
