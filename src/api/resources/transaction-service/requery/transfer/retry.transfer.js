602e340b3b22492ddf702cf84021d43a8bf58853
Meshileya seun <meshileyaseun@gmail.com>
2021-01-26 11:30:02 +0100

    requery tnx

--- src/api/resources/transaction-service/requery/transfer/retry.transfer.js
+++ src/api/resources/transaction-service/requery/transfer/retry.transfer.js
@@ -86,7 +86,7 @@ function reQueryPendingTransfer(callback) {
 
 // run job every one minutes
 export const RetryTransferJob = (): CronJob => {
-  return new CronJob("0 */1 * * * *", function () {
+  return new CronJob("0 */2 * * * *", function () {
     const formattedDate = moment.tz("Africa/Lagos");
     logger.info(`::: reQuery for transfer started ${formattedDate} :::`);
 
