0d5c8b66063a99611d41d59fb50632040ab5ef2f
Meshileya Seun <meshileyaseun@gmail.com>
2022-04-02 20:07:39 +0100

    Update retry.transfer.js

--- src/api/resources/transaction-service/requery/transfer/retry.transfer.js
+++ src/api/resources/transaction-service/requery/transfer/retry.transfer.js
@@ -37,7 +37,7 @@ async function reQueryPendingTransfer() {
       "WHERE (status.name = $1 OR status.name = $2 OR status.name = $3) " +
       "AND (type.name = $4 OR type.name = $5) " +
       "AND tnx.time_created >= $6 AND tnx.time_created <= $7 " +
-      "ORDER BY tnx.time_created ASC limit 100",
+      "ORDER BY tnx.time_created DESC limit 100",
 
     values: [
       pendingTransactionStatus,
